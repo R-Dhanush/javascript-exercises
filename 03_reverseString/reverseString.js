4462e9f1d7fcae0af126c2ecb65698054947ac45
R-Dhanush <dhanu72004@gmail.com>
2025-01-31 13:08:15 +0530

    completed the task reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('');
+    array.reverse();
+    let stringReverse = array.join('');
+    return stringReverse;
+    
 };
 
 // Do not edit below this line
