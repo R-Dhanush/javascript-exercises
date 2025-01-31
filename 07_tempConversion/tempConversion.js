6a9dbc9fc3dd9953a363ef7118b9b20245ae642b
R-Dhanush <dhanu72004@gmail.com>
2025-01-31 17:00:22 +0530

    The task tempConversion completed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let result = ((x - 32) * 5 / 9);
+  let celsius = Math.round(result * 10) /10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let result = ((x * 9 / 5) + 32);
+  let fahrenheit = Math.round(result * 10) /10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
