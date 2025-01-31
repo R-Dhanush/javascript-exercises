39707344841a8bbe6d5ccc1e5c53c9b16fe294c4
R-Dhanush <dhanu72004@gmail.com>
2025-01-31 12:58:06 +0530

    completed repeatstring task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string , num) {
+    let concatString = '';
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        if (string == ''){
+            return '';
+        }
+        else {
+            for (let i = 1; i <= num; i++){
+                concatString = concatString + string;
+            }
+            return concatString;
+        }
+    }
+    
 };
 
 // Do not edit below this line
